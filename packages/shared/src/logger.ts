15d01e5ea0648e0209f4628d2762579995f17fd4
Biggie Cheese <Jv3studios@gmail.com>
2025-11-03 14:23:39 -0600

    added picocolors to terminal output

--- packages/shared/src/logger.ts
+++ packages/shared/src/logger.ts
@@ -1,6 +1,8 @@
+import pc from 'picocolors';
+
 export const log = {
-  info: (msg: string) => console.log(`\x1b[36m[pyra]\x1b[0m ${msg}`),
-  success: (msg: string) => console.log(`\x1b[32m[pyra]\x1b[0m ${msg}`),
-  warn: (msg: string) => console.warn(`\x1b[33m[pyra]\x1b[0m ${msg}`),
-  error: (msg: string) => console.error(`\x1b[31m[pyra]\x1b[0m ${msg}`),
+  info: (msg: string) => console.log(`${pc.cyan('[pyra]')} ${msg}`),
+  success: (msg: string) => console.log(`${pc.green('[pyra]')} ${msg}`),
+  warn: (msg: string) => console.warn(`${pc.yellow('[pyra]')} ${msg}`),
+  error: (msg: string) => console.error(`${pc.red('[pyra]')} ${msg}`),
 };
