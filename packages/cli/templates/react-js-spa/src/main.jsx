a94beaf6b2460f87904f70540380d0c6f3a950cc
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-02-19 13:10:31 -0600

    feat: added an App.jsx

--- packages/cli/templates/react-js-spa/src/main.jsx
+++ packages/cli/templates/react-js-spa/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './style.css';
 
-createRoot(document.getElementById('app')!).render(
+createRoot(document.getElementById('app')).render(
   <StrictMode>
     <App />
   </StrictMode>
