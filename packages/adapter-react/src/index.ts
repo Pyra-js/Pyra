f2efddf1f11c153dda99c1a65baa1d6f6a09df37
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-03-04 00:51:00 -0600

    chore: exported the new changes in adapter-react

--- packages/adapter-react/src/index.ts
+++ packages/adapter-react/src/index.ts
@@ -2,5 +2,7 @@ export { createReactAdapter } from "./adapter.js";
 export { Image } from "./Image.js";
 export type { ImageProps } from "./Image.js";
 export { Link } from "./Link.js";
-export { useLocation } from "./hooks.js";
+export { NavLink } from "./NavLink.js";
+export type { NavLinkProps } from "./NavLink.js";
+export { useLocation, useNavigate, useSearchParams, useParams, useNavigating } from "./hooks.js";
 export type { Location } from "./hooks.js";
