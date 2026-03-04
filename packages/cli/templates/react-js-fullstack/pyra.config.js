1ade381e343cd1b9268ece2d4290ca3164052b5f
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-03-03 23:07:50 -0600

    feat: added the createReactAdapter to the react-js fullstack template

--- packages/cli/templates/react-js-fullstack/pyra.config.js
+++ packages/cli/templates/react-js-fullstack/pyra.config.js
@@ -1,4 +1,7 @@
+import { createReactAdapter } from '@pyra-js/adapter-react';
+
 /** @type {import('@pyra-js/shared').PyraConfig} */
 export default {
+  adapter: createReactAdapter(),
   routesDir: 'src/routes',
 };
