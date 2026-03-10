b0faf9dbfe56a12140c321721bd1f6a21518df17
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-03-09 21:11:24 -0500

    style: added correct createReactAdapter in examples/routing-showcase pyra.config.ts

--- examples/routing-showcase/pyra.config.ts
+++ examples/routing-showcase/pyra.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from '@pyra-js/cli';
+import { createReactAdapter } from '@pyra-js/adapter-react';
 
 export default defineConfig({
+  adapter: createReactAdapter(),
   routesDir: 'src/routes',
 });
