a242f958c19850b0001a75c85977ff979cd8dc8b
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-03-09 21:10:13 -0500

    chore: removed dead test code

--- examples/test-config/build/index.js
+++
@@ -1,2 +0,0 @@
-function o(n,r){return n+r}console.log("Hello from Pyra! \u{1F525}");console.log("Configuration is working!");var e=o(2,3);console.log(`2 + 3 = ${e}`);
-//# sourceMappingURL=index.js.map
